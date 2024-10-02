75945e848dd9b680239d036790d76d32a57b16f8
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-10-02 01:26:20 -0700

    feat: WeekCalendar display form error when appropriate

--- src/components/CreateEventForm/WeekCalendar.tsx
+++ src/components/CreateEventForm/WeekCalendar.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const dates = ['日', '月', '火', '水', '木', '金', '土'];
 
 interface WeekCalendarProps {
@@ -8,6 +6,8 @@ interface WeekCalendarProps {
 }
 
 const WeekCalendar: React.FC<WeekCalendarProps> = ({ selectedDays, setSelectedDays }) => {
+  const showError = selectedDays.length === 0;
+
   function toggleWeekday(index: number) {
     const newSelectedDays = selectedDays.includes(index)
       ? selectedDays.filter((day) => day !== index)
@@ -16,20 +16,32 @@ const WeekCalendar: React.FC<WeekCalendarProps> = ({ selectedDays, setSelectedDa
   }
 
   return (
-    <div className="flex mt-1 max-w-md rounded border-gray-300 border-t border-b border-l">
-      {dates.map((day, index) => (
-        <button
-          key={day}
-          type="button"
-          onClick={() => toggleWeekday(index)}
-          className={`py-2 w-full 
-            border-gray-300 border-r hover:bg-gray-200 ${
-              selectedDays.includes(index) ? 'bg-primary text-white hover:bg-primary' : ''
-            }`}
-        >
-          {day}
-        </button>
-      ))}
+    <div className="max-w-md w-full">
+      <label className="text-xl font-medium">曜日を選択</label>
+      <p 
+        className={`mb-4 text-red-500 ${showError ? '' : 'invisible'}`}
+      >
+        少なくとも1日を選択してください'
+      </p>
+      <div 
+        className={`flex w-full mt-4 rounded ${
+          showError ? ' border-2 border-red-500' : 'border-gray-300 border-t border-b border-l'
+        }`}
+      >
+        {dates.map((day, index) => (
+          <button
+            key={day}
+            type="button"
+            onClick={() => toggleWeekday(index)}
+            className={`py-2 w-full 
+              border-gray-300 border-r hover:bg-gray-200 ${
+                selectedDays.includes(index) ? 'bg-primary text-white hover:bg-primary' : ''
+              }`}
+          >
+            {day}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
