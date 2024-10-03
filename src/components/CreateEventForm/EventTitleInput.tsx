376dbfa656757049fc11ea2eef446e7b4ebeae9f
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-10-02 21:05:45 -0700

    feat: Title input display error when empty

--- src/components/CreateEventForm/EventTitleInput.tsx
+++ src/components/CreateEventForm/EventTitleInput.tsx
@@ -1,9 +1,15 @@
+import { useRef } from 'react';
+
 interface EventTitleInputProps {
   value: string;
   onChange: (value: string) => void;
 }
 
 const EventTitleInput: React.FC<EventTitleInputProps> = ({ value, onChange }) => {
+  const isInteracted = useRef(false);
+
+  const showError = isInteracted.current && value.length === 0;
+
   return (
     <div className="max-w-md w-full">
       <label htmlFor="eventTitle" className="text-xl font-medium">
@@ -13,10 +19,16 @@ const EventTitleInput: React.FC<EventTitleInputProps> = ({ value, onChange }) =>
         type="text"
         id="eventTitle"
         value={value}
-        onChange={(e) => onChange(e.target.value)}
+        onChange={(e) => {
+          onChange(e.target.value);
+          isInteracted.current = true;
+        }}
         className="mt-4 px-4 py-2 w-full rounded-md border border-gray-300 shadow-sm"
         placeholder="例) テニスサークル・ボウリング大会"
       />
+      <p className={`mt-2 px-4 text-red-500 ${showError ? '' : 'hidden'}`}>
+        イベント名を入力してください
+      </p>
     </div>
   );
 };
