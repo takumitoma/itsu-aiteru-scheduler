9de3e776196dfc23979383722d3d6c4b819a46f4
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-10-06 23:41:22 -0700

    fix: minor changes

--- src/app/api/event/route.ts
+++ src/app/api/event/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import { get } from './get';
 import { post } from './post';
 import { EventData } from '@/types/EventData';
--- src/components/CreateEventForm/Calendar.tsx
+++ src/components/CreateEventForm/Calendar.tsx
@@ -234,7 +234,8 @@ const Calendar: React.FC<CalendarProps> = ({
             >
               {day && (
                 <div
-                  className={`py-2 focus:outline-none hover:brightness-90 focus:ring-2 focus:ring-primary
+                  className={`py-2 focus:outline-none hover:brightness-90 focus:ring-2 
+                    focus:ring-primary 
                     ${day.isSame(dayjs().tz(timezone), 'day') ? 'font-bold' : ''} ${
                       selectedDates.includes(day.format('YYYY-MM-DD'))
                         ? isUnselectingRef.current && isDateInDragRange(day)
