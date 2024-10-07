f18b23ea0666e7144501d9523df9a44d96362d5e
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-10-07 11:19:10 -0700

    feat: add Participant name input field

--- src/components/ViewEvent/EditAvailabitiesButtons.tsx
+++ src/components/ViewEvent/EditAvailabitiesButtons.tsx
@@ -1,4 +1,6 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
+import { RxCross1 } from 'react-icons/rx';
+import { IoInformationCircleOutline } from 'react-icons/io5';
 
 interface EditAvailabitiesButtonsProps {
   isEditing: boolean;
@@ -9,15 +11,49 @@ const EditAvailabitiesButtons: React.FC<EditAvailabitiesButtonsProps> = ({
   isEditing,
   setIsEditing,
 }) => {
+  const [participantName, setParticipantName] = useState('');
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  // used to unfocus buttons on click
+  const popupButtonRef = useRef<HTMLButtonElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
 
   function toggleIsEditing() {
     buttonRef.current?.blur();
+    setIsPopupOpen(true);
     setIsEditing((prev) => !prev);
   }
 
   function cancelEditing() {
     setIsEditing(false);
+  }
+
+  function cancelNameChange() {
+    setParticipantName('');
+    setShowError(false);
+    setIsPopupOpen(false);
+  }
+
+  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setParticipantName(e.target.value);
+    if (showError && e.target.value) {
+      setShowError(false);
+    }
+  }
+
+  function handlePopupSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    popupButtonRef.current?.blur();
+    if (!participantName.trim()) {
+      setShowError(true);
+    } else {
+      // todo: handle user creation
+      console.log('Submitting participant name:', participantName);
+      setIsPopupOpen(false);
+      setShowError(false);
+      setParticipantName('');
+    }
   }
 
   return (
@@ -42,6 +78,56 @@ const EditAvailabitiesButtons: React.FC<EditAvailabitiesButtonsProps> = ({
       >
         <p className="select-none">{isEditing ? '保存' : '空き時間を追加'}</p>
       </button>
+      {isPopupOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center 
+            z-50 px-2"
+        >
+          <form
+            onSubmit={handlePopupSubmit}
+            className="bg-background p-6 rounded-md max-w-md w-full space-y-4"
+          >
+            <div className="flex justify-between">
+              <label htmlFor="participantName" className="text-xl font-medium">
+                名前
+              </label>
+              <button onClick={cancelNameChange} type="button">
+                <RxCross1 size={24} />
+              </button>
+            </div>
+            <div className="flex">
+              <IoInformationCircleOutline className="translate-y-[1px]" />
+              <p className="text-xs">
+                再度アクセスする場合、同じ名前を入力して回答を編集してください
+              </p>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="participantName"
+                value={participantName}
+                onChange={handleNameChange}
+                placeholder={'例) やまだ'}
+                className={`px-4 py-2 w-full rounded-md shadow-sm bg-primaryVeryLight 
+                  text-customBlack focus:outline-none focus:ring-2 focus:ring-primary border ${
+                    showError ? 'border-red-500' : 'border-primary'
+                  }`}
+              />
+              {showError && <p className="text-red-500 mt-1">名前を入力してください</p>}
+            </div>
+            <div className="flex justify-end">
+              <button
+                ref={popupButtonRef}
+                type="submit"
+                className="text-white bg-primary px-4 py-2 rounded-md flex-shrink-0 
+                  hover:bg-primaryHover focus:bg-primaryHover"
+              >
+                追加
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
