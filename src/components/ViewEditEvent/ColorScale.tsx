aec0fc4d77dbd4e0a2b85f7ad0c29b0a9f778ea9
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-10-14 23:57:52 -0700

    refactor: improve scalability for ColorScale

--- src/components/ViewEditEvent/ColorScale.tsx
+++ src/components/ViewEditEvent/ColorScale.tsx
@@ -1,20 +1,31 @@
+const MAX_VISIBLE_COLORS = 20;
+
 interface ColorScaleProps {
   colorScale: string[];
   numParticipants: number;
 }
 
 const ColorScale: React.FC<ColorScaleProps> = ({ colorScale, numParticipants }) => {
+  function getDisplayColors() {
+    if (colorScale.length <= MAX_VISIBLE_COLORS) {
+      return colorScale;
+    }
+
+    const groupSize = Math.ceil(colorScale.length / MAX_VISIBLE_COLORS);
+    return colorScale.filter((_, index) => index % groupSize === 0);
+  }
+
+  const displayColors = getDisplayColors();
+
   return (
     <section className="flex w-full space-x-1">
-      <span className="whitespace-nowrap">{`0/${numParticipants} 人`}</span>
+      <span className="whitespace-nowrap">{`0 人`}</span>
       <div className="flex w-full border border-foreground">
-        {colorScale.map((color, index) => (
-          <div key={index} className="w-full" style={{ backgroundColor: color }}>
-            &nbsp;
-          </div>
+        {displayColors.map((color, index) => (
+          <div key={index} className="w-full h-6" style={{ backgroundColor: color }} />
         ))}
       </div>
-      <span className="whitespace-nowrap">{`${numParticipants}/${numParticipants} 人`}</span>
+      <span className="whitespace-nowrap">{`${numParticipants} 人`}</span>
     </section>
   );
 };
