68034bcea4b3e9362f82a181c36351de20e9df25
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-11-01 19:57:37 -0700

    fix: remove unnecessary files

--- src/app/[eventId]/loading.tsx
+++
@@ -1,3 +0,0 @@
-import Loading from '@/components/Loading/Loading';
-
-export default Loading;
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,12 @@
-import Loading from '@/components/Loading/Loading';
+const Loading = () => {
+  return (
+    <h1
+      className="h-[calc(100vh-5rem)] flex items-center justify-center 
+        font-bold text-xl sm:text-4xl"
+    >
+      読み込み中...
+    </h1>
+  );
+};
 
 export default Loading;
