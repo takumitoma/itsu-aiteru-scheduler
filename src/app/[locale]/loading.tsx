402937a377637aba3fed3afe2fa8d4704d634787
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-11-13 21:01:02 -0800

    feat: add i18n support to Layout, Loading, and Not Found page

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,12 +1,14 @@
-const Loading = () => {
+import { getTranslations } from 'next-intl/server';
+
+export default async function Loading() {
+  const t = await getTranslations('Loading');
+
   return (
     <h1
       className="h-[calc(100vh-5rem)] flex items-center justify-center 
         font-bold text-xl sm:text-4xl"
     >
-      読み込み中...
+      {t('message')}
     </h1>
   );
-};
-
-export default Loading;
+}
