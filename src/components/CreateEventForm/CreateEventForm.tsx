5890c7bef50fd6bbdff5cd06138dbe54c43aace7
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-11-14 22:32:56 -0800

    fix: routing error caused by not using next-intl useRouter

--- src/components/CreateEventForm/CreateEventForm.tsx
+++ src/components/CreateEventForm/CreateEventForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useRef } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
 import EventTitleInput from './EventTitleInput';
 import SurveyTypeSelector from './SurveyTypeSelector';
