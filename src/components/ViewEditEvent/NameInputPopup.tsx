2fcb39b3c82a4cd41da366bb00e763992da1adb7
tsugita26 <tsugita@ucsd.edu>
2025-01-26 21:38:38 +0900

    fix: linting errors

--- src/components/ViewEditEvent/NameInputPopup.tsx
+++ src/components/ViewEditEvent/NameInputPopup.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { RxCross1 } from 'react-icons/rx';
 import { useTranslations } from 'next-intl';
 import { zodResolver } from '@hookform/resolvers/zod';
