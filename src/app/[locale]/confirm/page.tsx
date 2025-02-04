5ab70c888746f1488360c3be4d31ab6f60206754
tsugita26 <tsugita@ucsd.edu>
2025-02-04 17:39:19 +0900

    feat: add confirm email page

--- src/app/[locale]/confirm/page.tsx
+++ src/app/[locale]/confirm/page.tsx
@@ -1,7 +1,23 @@
-export default function ConfirmPage() {
+import { getTranslations } from 'next-intl/server';
+
+export default async function ConfirmEmailPage() {
+  const t = await getTranslations('ConfirmEmail');
+
   return (
-    <div>
-      <h1>Your email has been confirmed</h1>
-    </div>
+    <section className="mx-auto flex w-full max-w-md flex-col items-center justify-center space-y-8">
+      <h1 className="underline decoration-primary decoration-4 underline-offset-[16px]">
+        {t('title')}
+      </h1>
+      <section className="w-full space-y-4">
+        <p>{t('description')}</p>
+        <p>{t('newFeatures')}</p>
+        <ul className="ml-8 list-outside list-disc space-y-2">
+          <li>{t('featureNotification')}</li>
+          <li>{t('featurePassword')}</li>
+          <li>{t('featureHistory')}</li>
+        </ul>
+        <p className="mt-4 text-sm text-gray-600">{t('comingSoon')}</p>
+      </section>
+    </section>
   );
 }
