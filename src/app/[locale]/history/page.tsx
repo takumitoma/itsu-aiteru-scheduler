8d59dc2b347757f528e2ec151053fc3ee9e32e9d
tsugita26 <tsugita@ucsd.edu>
2025-02-12 21:23:49 +0900

    feat: add more details to events on history page

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -3,9 +3,12 @@ import { FaRegTrashAlt } from 'react-icons/fa';
 type Event = {
   id: string;
   title: string;
-  dates: string[];
-  time: string;
-  participants: number;
+  dates: string[] | null;
+  daysOfWeek: number[] | null;
+  timeRangeStart: number;
+  timeRangeEnd: number;
+  numParticipants: number;
+  createdAt: Date;
   url: string;
 };
 
@@ -14,24 +17,33 @@ const events: Event[] = [
     id: '1',
     title: 'Event 1',
     dates: ['4/1/2025', '4/2/2025', '4/3/2025'],
-    time: '9AM - 5PM',
-    participants: 16,
+    daysOfWeek: null,
+    timeRangeStart: 9,
+    timeRangeEnd: 18,
+    numParticipants: 16,
+    createdAt: new Date(2025, 4, 1),
     url: 'http://hello.com',
   },
   {
     id: '2',
     title: 'Event 2',
     dates: ['4/1/2025', '4/2/2025', '4/3/2025'],
-    time: '9AM - 1PM',
-    participants: 1,
+    daysOfWeek: null,
+    timeRangeStart: 9,
+    timeRangeEnd: 13,
+    numParticipants: 1,
+    createdAt: new Date(2025, 2, 5),
     url: 'http://hello.com',
   },
   {
     id: '3',
     title: 'Event 3',
     dates: ['4/1/2025', '4/2/2025', '4/3/2025'],
-    time: '9AM - 9PM',
-    participants: 12,
+    daysOfWeek: null,
+    timeRangeStart: 10,
+    timeRangeEnd: 21,
+    numParticipants: 12,
+    createdAt: new Date(2025, 3, 25),
     url: 'http://hello.com',
   },
 ];
@@ -43,15 +55,20 @@ export function EventCard({ event }: { event: Event }) {
       <dl>
         <div className="flex space-x-1">
           <dt className="font-medium">Event dates:</dt>
-          <dd>{event.dates.join(', ')}</dd>
+          {event.dates && <dd>{event.dates.join(', ')}</dd>}
+          {event.daysOfWeek && <dd>{event.daysOfWeek.join(', ')}</dd>}
         </div>
         <div className="flex space-x-1">
           <dt className="font-medium">Event time:</dt>
-          <dd>{event.time}</dd>
+          <dd>{`${event.timeRangeStart} - ${event.timeRangeEnd}`}</dd>
         </div>
         <div className="flex space-x-1">
           <dt className="font-medium">Number of participants:</dt>
-          <dd>{event.participants}</dd>
+          <dd>{event.numParticipants}</dd>
+        </div>
+        <div className="flex space-x-1">
+          <dt className="font-medium">Created at:</dt>
+          <dd>{event.createdAt.toDateString()}</dd>
         </div>
         <div className="flex space-x-1">
           <dt className="font-medium">Event URL:</dt>
