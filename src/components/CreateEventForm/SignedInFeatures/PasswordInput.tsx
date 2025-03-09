2f7f3680f932f2aaeaab1520334d9cee7ea2904d
tsugita26 <tsugita@ucsd.edu>
2025-03-09 21:58:44 +0900

    feat: CreateEventForm show password functionality

--- src/components/CreateEventForm/SignedInFeatures/PasswordInput.tsx
+++ src/components/CreateEventForm/SignedInFeatures/PasswordInput.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { useTranslations } from 'next-intl';
 import { type UseFormRegisterReturn } from 'react-hook-form';
+
+import { BiSolidShow, BiSolidHide } from 'react-icons/bi';
 
 interface PasswordInputProps {
   className: string;
@@ -9,13 +12,24 @@ interface PasswordInputProps {
 export function PasswordInput({ className, register }: PasswordInputProps) {
   const t = useTranslations('CreateEvent.SignedInFeatures.PasswordInput');
 
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className={`flex w-full flex-col space-y-4 ${className}`}>
-      <label htmlFor="password" className="">
-        {t('label')}
-      </label>
+      <div className="flex justify-between">
+        <label htmlFor="password" className="">
+          {t('label')}
+        </label>
+        <button
+          type="button"
+          onClick={() => setShowPassword((prev) => !prev)}
+          className="rounded-full p-1 hover:bg-grayCustom"
+        >
+          {showPassword ? <BiSolidHide size={24} /> : <BiSolidShow size={24} />}
+        </button>
+      </div>
       <input
-        type="password"
+        type={showPassword ? 'text' : 'password'}
         maxLength={16}
         id="password"
         className="border border-primary"
