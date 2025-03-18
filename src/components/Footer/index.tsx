07101fdbd23af6840e469cc3abf2eea6717a5c6d
tsugita26 <tsugita@ucsd.edu>
2025-03-18 23:51:39 +0900

    feat: add url to demo event in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,10 @@
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 import { usePathname } from '@/i18n/routing';
+
 import { AppSettings } from './AppSettings';
 import { FooterContact } from './FooterContact';
+import { Demo } from './Demo';
 
 export function Footer() {
   const t = useTranslations('Footer');
@@ -19,23 +21,20 @@ export function Footer() {
           <span className="text-xl font-bold">{t('appName')}</span>
         </div>
 
-        <div className="flex flex-col space-y-8 lg:flex-row lg:justify-between lg:space-y-0">
-          <div className="flex flex-col space-y-8 md:flex-row md:justify-between md:space-x-8 md:space-y-0">
-            {/* About */}
-            <article className="flex w-full flex-col space-y-4 lg:w-[320px]">
-              <h4 className="font-semibold">{t('About.title')}</h4>
-              <p className="text-sm">{t('About.description')}</p>
-            </article>
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <article className="flex w-full flex-col space-y-4">
+            <h4 className="font-semibold">{t('About.title')}</h4>
+            <p className="text-sm">{t('About.description')}</p>
+          </article>
 
-            {/* Contact */}
-            {pathname !== '/contact' && (
-              <section className="w-full lg:w-[320px]">
-                <FooterContact />
-              </section>
-            )}
+          {pathname !== '/guide' && <Demo />}
+
+          {pathname !== '/contact' && <FooterContact />}
+
+          {/* for xl screens, always on the right regardless of conditional renders above */}
+          <div className="xl:col-start-4">
+            <AppSettings />
           </div>
-
-          <AppSettings />
         </div>
       </div>
     </footer>
